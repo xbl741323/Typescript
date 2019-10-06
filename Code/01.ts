e8599b641aa76bb568fde969e8ae4d65cd131dcf
xbl <1546766776@qq.com>
2019-10-06 17:41:54 +0800

    Typescript学习笔记和代码

---
+++ Code/01.ts
@@ -0,0 +1,194 @@
+console.log("你好，Typescript!")
+
+//布尔类型
+var flag: boolean = true;
+flag = false;
+console.log(flag)
+
+//数字类型
+var num: number = 123;
+num = 456;
+console.log(num)
+
+//字符串类型
+var str: string = "你好"
+str = "haha"
+console.log(str)
+
+//数组类型
+//第一种表示方式
+var arr: number[] = [1, 2, 3, 4, 5]
+console.log(arr.toString())
+
+//第二种表示方式
+var arr2: Array<number> = [1, 2, 3, 4, 5]
+console.log(arr2.toString())
+
+//第三种表示方式
+var arr6: any = [1, 'zs', 3, true, 5]
+console.log(arr6.toString())
+
+//元组类型
+var arr3: [number, number, string] = [234, 123, 'hello']
+console.log(arr3.toString())
+
+//枚举类型
+enum Flag { success = 1, error = 3 }
+let c: Flag = Flag.error;
+console.log(c)
+
+enum Color { blue, red, 'orange' }
+let e: Color = Color.red;
+let x: Color = Color.orange
+console.log(e)//会输出对应下标
+console.log(x)
+
+enum Err { 'undefined' = -1, 'null' = -2, 'success' = 1 }
+let s: Err = Err.null
+console.log(s)
+
+//任意类型（any）
+var num1: any = 123;
+num1 = 'str'
+console.log(num1)
+
+var obj: any = document.getElementById("content")
+obj.style.color = "red";
+
+//null 和 undefined 类型
+var result: number | undefined;
+result = 123;
+console.log(result)
+
+var result1: null | undefined;
+console.log(result1)
+
+var result3: null;
+result3 = null
+console.log(result3)
+
+var result4: number | null | undefined;
+result4 = 123
+console.log(result4)
+
+//void类型
+function run(): void {
+    console.log('run')
+}
+run()
+
+//Typescript 函数
+//1.函数声明法
+function fun(): number {
+    return 123;
+}
+console.log(fun())
+//2.函数表达式法（匿名函数法）
+var fun2 = function (): string {
+    return 'zs'
+}
+console.log(fun2())
+
+//Ts中定义方法传参
+function getInfo(name: string, age: number): string {
+    return `我叫${name}，今年${age}岁了`//这里用到了模板自变量表示法
+}
+
+console.log(getInfo('zs', 18))
+
+//没有返回值的方法
+function getInfo2(name: string, age: number): void {
+    console.log(name + age)
+}
+getInfo2('ls', 20)
+
+//方法可选参数
+//Es5里面方法的实参和形参可以不一样，如果不一样就需要配置可选参数
+
+function getPersonalInfo(name: string, age?: number): string {
+    if (age) {
+        return `${name}---${age}`
+    } else {
+        return `${name}---年龄不告诉你`
+    }
+}
+
+console.log(getPersonalInfo('ww'))
+
+//注意：可选参数必须配置到参数的最后面
+/*
+function getPersonalInfo(name？: string, age: number): string {
+    if (age) {
+        return `${name}---${age}`
+    } else {
+        return `${name}---年龄不告诉你`
+    }
+}
+console.log(getPersonalInfo('ww'))
+以上这种写法是错误的
+*/
+
+//默认参数（默认参数也可以叫做可选参数）
+function getPersonalInfo2(name: string, age: number = 20): string {
+    if (age) {
+        return `${name}---${age}`
+    } else {
+        return `${name}---年龄不告诉你`
+    }
+}
+console.log(getPersonalInfo2('lyy', 30))//调用方法时传入默认参数的值会覆盖原来默认参数的值
+
+//三点运算符（用于解决形参个数太多不好表示的问题）
+function add(...result: number[]): number {
+    var sum = 0;
+    result.forEach(item => {
+        sum += item
+    })
+    return sum;
+}
+console.log(add(1, 2, 3, 4, 5))
+
+function add2(num: number, ...result: number[]): number {
+    var sum = num;
+    result.forEach(item => {
+        sum += item
+    })
+    return sum;
+}
+console.log(add(1, 2, 3, 4, 5))
+
+//Ts函数重载
+// java中方法的重载，重载指的是方法的名字一样，但是形参的类型，个数，排列顺序不同，这是
+//会出现重载的情况
+
+// Typescript中的重载，通过为同一个同名函数提供多个函数类型来实现多种功能的目的
+
+// Ts为了兼容Es5  以及 Es6 重载的写法和java中有区别
+
+//Es5中出现同名函数，后面的函数会直接覆盖前面的函数
+/*
+function test(num){
+
+}
+
+function test(num,num2){
+
+}
+*/
+
+//Ts中函数的重载写法如下所示
+function postInfo(name: string): string;
+
+function postInfo(age: number): number;
+
+function postInfo(arr: any): any {
+    if (typeof (arr) == 'string') {
+        return `我的名字是${arr}`
+    } else {
+        return `我的年龄是${arr}`
+    }
+}
+console.log(postInfo('zs'))
+console.log(postInfo(13))
+
+
---
+++ Code/js/01.js
@@ -0,0 +1,166 @@
+"use strict";
+console.log("你好，Typescript!");
+//布尔类型
+var flag = true;
+flag = false;
+console.log(flag);
+//数字类型
+var num = 123;
+num = 456;
+console.log(num);
+//字符串类型
+var str = "你好";
+str = "haha";
+console.log(str);
+//数组类型
+//第一种表示方式
+var arr = [1, 2, 3, 4, 5];
+console.log(arr.toString());
+//第二种表示方式
+var arr2 = [1, 2, 3, 4, 5];
+console.log(arr2.toString());
+//第三种表示方式
+var arr6 = [1, 'zs', 3, true, 5];
+console.log(arr6.toString());
+//元组类型
+var arr3 = [234, 123, 'hello'];
+console.log(arr3.toString());
+//枚举类型
+var Flag;
+(function (Flag) {
+    Flag[Flag["success"] = 1] = "success";
+    Flag[Flag["error"] = 3] = "error";
+})(Flag || (Flag = {}));
+var c = Flag.error;
+console.log(c);
+var Color;
+(function (Color) {
+    Color[Color["blue"] = 0] = "blue";
+    Color[Color["red"] = 1] = "red";
+    Color[Color["orange"] = 2] = "orange";
+})(Color || (Color = {}));
+var e = Color.red;
+var x = Color.orange;
+console.log(e); //会输出对应下标
+console.log(x);
+var Err;
+(function (Err) {
+    Err[Err["undefined"] = -1] = "undefined";
+    Err[Err["null"] = -2] = "null";
+    Err[Err["success"] = 1] = "success";
+})(Err || (Err = {}));
+var s = Err.null;
+console.log(s);
+//任意类型（any）
+var num1 = 123;
+num1 = 'str';
+console.log(num1);
+var obj = document.getElementById("content");
+obj.style.color = "red";
+//null 和 undefined 类型
+var result;
+result = 123;
+console.log(result);
+var result1;
+console.log(result1);
+var result3;
+result3 = null;
+console.log(result3);
+var result4;
+result4 = 123;
+console.log(result4);
+//void类型
+function run() {
+    console.log('run');
+}
+run();
+//Typescript 函数
+//1.函数声明法
+function fun() {
+    return 123;
+}
+console.log(fun());
+//2.函数表达式法（匿名函数法）
+var fun2 = function () {
+    return 'zs';
+};
+console.log(fun2());
+//Ts中定义方法传参
+function getInfo(name, age) {
+    return "\u6211\u53EB" + name + "\uFF0C\u4ECA\u5E74" + age + "\u5C81\u4E86"; //这里用到了模板自变量表示法
+}
+console.log(getInfo('zs', 18));
+//没有返回值的方法
+function getInfo2(name, age) {
+    console.log(name + age);
+}
+getInfo2('ls', 20);
+//方法可选参数
+//Es5里面方法的实参和形参可以不一样，如果不一样就需要配置可选参数
+function getPersonalInfo(name, age) {
+    if (age) {
+        return name + "---" + age;
+    }
+    else {
+        return name + "---\u5E74\u9F84\u4E0D\u544A\u8BC9\u4F60";
+    }
+}
+console.log(getPersonalInfo('ww'));
+//注意：可选参数必须配置到参数的最后面
+/*
+function getPersonalInfo(name？: string, age: number): string {
+    if (age) {
+        return `${name}---${age}`
+    } else {
+        return `${name}---年龄不告诉你`
+    }
+}
+console.log(getPersonalInfo('ww'))
+以上这种写法是错误的
+*/
+//默认参数（默认参数也可以叫做可选参数）
+function getPersonalInfo2(name, age) {
+    if (age === void 0) { age = 20; }
+    if (age) {
+        return name + "---" + age;
+    }
+    else {
+        return name + "---\u5E74\u9F84\u4E0D\u544A\u8BC9\u4F60";
+    }
+}
+console.log(getPersonalInfo2('lyy', 30)); //调用方法时传入默认参数的值会覆盖原来默认参数的值
+//三点运算符（用于解决形参个数太多不好表示的问题）
+function add() {
+    var result = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        result[_i] = arguments[_i];
+    }
+    var sum = 0;
+    result.forEach(function (item) {
+        sum += item;
+    });
+    return sum;
+}
+console.log(add(1, 2, 3, 4, 5));
+function add2(num) {
+    var result = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        result[_i - 1] = arguments[_i];
+    }
+    var sum = num;
+    result.forEach(function (item) {
+        sum += item;
+    });
+    return sum;
+}
+console.log(add(1, 2, 3, 4, 5));
+function postInfo(arr) {
+    if (typeof (arr) == 'string') {
+        return "\u6211\u7684\u540D\u5B57\u662F" + arr;
+    }
+    else {
+        return "\u6211\u7684\u5E74\u9F84\u662F" + arr;
+    }
+}
+console.log(postInfo('zs'));
+console.log(postInfo(13));
